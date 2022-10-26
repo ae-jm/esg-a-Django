23d2397fdcce827071aa3bc0e183c09d255fc1ef
ae-jm <py6556@gmail.com>
2022-10-26 16:12:10 +0900

    blog.index 후에 Post 목록 조회 추가

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from blog.models import Post
 
 def index(request):
     # 전체 포스팅을 가져올 준비. 아직 가져오지는 않음.
-    post_qs = Post.objects.all()
+    post_qs = Post.objects.all().order_by("-id")
     return render(request, 'blog/index.html', {
-        "post_list":post_qs
+        "post_list":post_qs,
     })
