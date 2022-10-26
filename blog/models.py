d78c983a58f4147c7551576b455b9164aab0e0c1
ae-jm <py6556@gmail.com>
2022-10-26 12:23:17 +0900

    single_pages 수정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_at = models.DateTimeField()
